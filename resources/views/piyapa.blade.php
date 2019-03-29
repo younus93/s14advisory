d0c0b21b46bc3de34e5304a5674c6615826e7148
younus93 <itsme@theyounus.com>
2019-03-30 03:25:32 +0530

    initial commit

---
+++ resources/views/piyapa.blade.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <title>S14Advisory</title>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="img/logos/logo-shortcut.png"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/themify-icons.css">
+    <link rel="stylesheet" type="text/css" href="/css/fontawesome-all.css">
+    <link rel="stylesheet" type="text/css" href="/css/icomoon.css">
+    <link rel="stylesheet" type="text/css" href="/css/plugins.css">
+    <link rel="stylesheet" href="/css/animate.css">
+    <link rel="stylesheet" href="/css/owl.carousel.css">
+    <link rel="stylesheet" href="/css/slider.css">
+    <link rel="stylesheet" type="text/css" href="/css/navigation.css" id="navigation_menu">
+    <link rel="stylesheet" type="text/css" href="/css/default.css">
+    <link rel="stylesheet" type="text/css" href="/css/styles.css" id="main_styles">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400i,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900"
+          rel="stylesheet">
+</head>
+
+<body>
+<div id="preloader">
+    <div class="row loader">
+        <div class="loader-icon"></div>
+    </div>
+</div>
+<div id="top-bar" class="hidden-md-down">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9 col-12">
+                <ul class="top-bar-info">
+                    <li><i class="fas fa-map-marker-alt"></i>Klongtoey, Bangkok 10100</li>
+                    <li><i class="fas fa-phone-square"></i> Phone: +1-23-456789</li>
+                    <li><i class="fa fa-envelope"></i>Email: info@s14advisory.com</li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-12">
+                <ul class="social-icons hidden-sm">
+                    <li><a href="#">EN</a></li>
+                    <li><a href="#">TH</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<header>
+    <nav id="navigation4" class="container navigation">
+        <div class="nav-header">
+            <a class="nav-brand" href="/">
+                <img src="/img/logos/blackLogo.png" style="max-height: 40px;" alt="logo" id="main_logo"></a>
+            <div class="nav-toggle"></div>
+        </div>
+        <div class="nav-menus-wrapper">
+            <ul class="nav-menu align-to-right">
+                <li>
+                    <a href="/">Return Home</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+<div class="section-block">
+    <div class="container">
+        <div class="project-single">
+            <div class="row">
+                <div class="col-md-8 col-sm-8 col-12">
+                    <div class="project-single-text pr-30-md"><h4>Ms. Piyapa Chongsathien</h4>
+                        <p>Piyapa Chongsathien is a founder and a managing director of S14 Advisory Company
+                            Limited. She was graduated from Assumption University with Bachelor and master degree in
+                            Business and Administration majoring in Finance and has over 17 years of work experiences
+                            investment banking business. Her experiences cover all areas of investment banking
+                            including M&amp;A, IPO, fund raising, IFA service, Share(or) Company valuation, restructurings
+                            and financial advisory works.</p>
+                        <h4 class="mt-30">Her past work includes</h4>
+                        <ul class="primary-list">
+                            <li><i class="fa fa-check-circle"></i>FA for IPO of CAZ (Thailand) Plc. in 2019(CAZ) (First day trade at Jan 22, 2019)</li>
+                            <li><i class="fa fa-check-circle"></i>FA for Tender offer of Digital Tech Planet Plc (DIGI) in 2019</li>
+                            <li><i class="fa fa-check-circle"></i>IFA for TAKUNI Group Plc (TAKUNI) in 2018</li>
+                            <li><i class="fa fa-check-circle"></i>IFA for Food Capitals Plc (FC) in 2018</li>
+                            <li><i class="fa fa-check-circle"></i>FA on M&amp;A transaction of BLISS-Tel Plc (BLISS) to acquire 35% of ITG Group in 2017</li>
+                            <li><i class="fa fa-check-circle"></i>IFA for Country Group Plc (CGD) in 2017</li>
+                            <li><i class="fa fa-check-circle"></i>IFA for CPL Group Plc(CPL) in 2017</li>
+                            <li><i class="fa fa-check-circle"></i>Valuation for Information Technology Company Limited in 2017</li>
+                            <li><i class="fa fa-check-circle"></i>Valuation for AQ Estate Public Company Limited (AQ) in 2017</li>
+                            <li><i class="fa fa-check-circle"></i>Valuation for Information Technology Group Company limited(ITG) in 2017</li>
+                            <li><i class="fa fa-check-circle"></i>Valuation for Electronic Industry Pcl (EIC) in 2016</li>
+                            <li><i class="fa fa-check-circle"></i>Valuation for Nathalin Company Limited in 2016</li>
+                            <li><i class="fa fa-check-circle"></i>Warrant for Siam Sport Syndicate Plc (SPORT) in 2016</li>
+                            <li><i class="fa fa-check-circle"></i>General Advisory Services for Pylon Plc (PYLON) in 2015</li>
+                            <li><i class="fa fa-check-circle"></i>General Advisory Services for General Beverage Company Limited in 2015</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4 col-12">
+                    <img src="/img/s14team/md.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer class="footer-style-1">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-6 col-12">
+                <h3>About Us</h3><a href="#"><img src="/img/logos/logo.png" style="max-height: 75px;" alt="img"></a>
+                <div class="mt-20">
+                    <p>Financial Advisory Services</p>
+                </div>
+                <ul class="footer-style-1-social-links">
+                    <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+                    <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
+                    <li><a href="#"><i class="fab fa-skype"></i></a></li>
+                </ul>
+            </div>
+            <div class="col-md-4 col-sm-6 col-12">
+                <h3>Contact Info</h3>
+                <ul class="footer-style-1-contact-info">
+                    <li><i class="fa fa-phone"></i> <span>+11 (0) 123 456 7890</span></li>
+                    <li><i class="fa fa-envelope-open"></i> <span>info@s14advisory.com</span></li>
+                    <li><i class="fa fa-map-marker-alt"></i> <span>Klongtoey, Bangkok 10100</span></li>
+                    <li><i class="fa fa-phone-square"></i> <span>+11 (0) 123 456 7890</span></li>
+                </ul>
+            </div>
+            <div class="col-md-4 col-sm-6 col-12">
+                <h3>Quick Links</h3>
+                <ul class="footer-style-1-links">
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Company History</a></li>
+                    <li><a href="#">Recent Posts</a></li>
+                    <li><a href="#">Our Services</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-style-1-bar">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-12">
+                    <h5>S14 Advisory Limited</h5>
+                </div>
+                <div class="col-md-6 col-sm-6 col-12">
+                    <ul class="footer-style-1-bar-links">
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<a href="#" class="scroll-to-top"><i class="fas fa-chevron-up"></i></a>
+<script src="/js/jquery.min.js"></script>
+<script src="/js/plugins.js"></script>
+<script src="/js/navigation.js"></script>
+<script src="/js/navigation.fixed.js"></script>
+<script src="/js/map.js"></script>
+<script src="/js/main.js"></script>
+<script>
+    $(document).on('click', 'a[href^="#"]', function (e) {
+        // target element id
+        var id = $(this).attr('href');
+
+        // target element
+        var $id = $(id);
+        if ($id.length === 0) {
+            return;
+        }
+
+        // prevent standard hash navigation (avoid blinking in IE)
+        e.preventDefault();
+
+        // top position relative to the document
+        var pos = $id.offset().top - 60;
+
+        // animated top scrolling
+        $('body, html').animate({scrollTop: pos}, 2000);
+    });
+</script>
+</body>
+
+</html>
---
+++ resources/views/scratch.blade.php
@@ -0,0 +1,36 @@
+<div class="row mt-40">
+    <div class="col-md-3 col-sm-3 col-12">
+        <div class="feature-block-2 feature-bordered feature-shadow feature-bg inner-40 left-holder">
+            <i class="icon-banking"></i>
+            <h4>Fund Raising</h4>
+            <p>Equity placement or recapitalization through our extensive network for a private placement or Pre initial public offering (IPO)</p>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-3 col-12">
+        <div class="feature-block-2 feature-bordered feature-shadow feature-bg inner-40 left-holder">
+            <i class="icon-businessman"></i>
+            <h4>Mergers & Acquistions</h4>
+            <p>Lead deals through our client network, recommend fair ranges of valuations.</p>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-3 col-12">
+        <div class="feature-block-2 feature-bordered feature-shadow feature-bg inner-40 left-holder"><i
+                    class="icon-airplane"></i>
+            <h4>Company (or) Share Valuation</h4>
+            <p>Provide services at all stages of preparing a valuation of share/company.</p>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-3 col-12">
+        <div class="feature-block-2 feature-bordered feature-shadow feature-bg inner-40 left-holder"><i
+                    class="icon-binoculars"></i>
+            <h4>IFA Service</h4>
+            <p>Provides independent analyses on financial perspective allows our clients to make fully informed business decisions.</p>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-3 col-12">
+        <div class="feature-block-2 feature-bordered feature-shadow feature-bg inner-40 left-holder"><i class="icon-bank"></i>
+            <h4>IPO advisory</h4>
+            <p>Provides services at all stages of preparing for an IPO and transforming a company from private into a public entity.</p>
+        </div>
+    </div>
+</div>
