52b5ba654afcaecb6c5245422d1d0d39d3c54766
younus93 <itsme@theyounus.com>
2019-04-10 13:16:55 +0530

    wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,7 +30,7 @@
 
     @include('values')
 
-    @include('services')
+    @include('services1')
 
     @include('team')
 
---
+++ resources/views/services1.blade.php
@@ -0,0 +1,125 @@
+@section('scripts')
+    @parent
+    <style>
+        .medium-icon-tabs-light .tabs .tab-menu li {
+            height: 135px;
+            width: 135px;
+        }
+
+        .medium-icon-tabs-light .tabs .tab-menu li a i {
+            color: #ffffff;
+        }
+        .medium-icon-tabs-light .tabs .tab-menu li a{
+            color: #ffffff;
+        }
+    </style>
+@append
+
+<div id="services" class="section-block black-overlay-60"
+     style="background-image: url('/img/homebg2.jpg');background-size: cover; z-index: 0;">
+    <div class="container">
+        <div class="large-heading center-holder">
+            <h5 class="white-color">Innovative Solutions to
+                <span class="italic libre-baskerville">Move Your business forward</span>.</h5>
+            <div class="section-heading-line"></div>
+        </div>
+        <div class="row">
+            <div class="col-md-10 col-sm-12 col-12 offset-md-1">
+                <div class="medium-icon-tabs-light">
+                    <div class="tabs tabs_animate mt-10">
+                        <ul class="tab-menu center-holder">
+                            <li class="active-tab">
+                                <a href="#tab-f-1">
+                                    <i class="icon-banking"></i>Fund <br>Raising
+                                </a>
+                            </li>
+                            <li class="">
+                                <a href="#tab-f-2">
+                                    <i class="icon-money-bag-1"></i>
+                                    Mergers &amp; Acquistions
+                                </a>
+                            </li>
+                            <li class="">
+                                <a href="#tab-f-3">
+                                    <i class="icon-analytics-3"></i>
+                                    Share <br> Valuation
+                                </a>
+                            </li>
+                            <li class="">
+                                <a href="#tab-f-4">
+                                    <i class="icon-balance"></i>
+                                    IFA <br>Service
+                                </a>
+                            </li>
+                            <li class="">
+                                <a href="#tab-f-5">
+                                    <i class="icon-cheque-1"></i>
+                                    IPO <br>Advisory
+                                </a>
+                            </li>
+                            <li class="">
+                                <a href="#tab-f-6">
+                                    <i class="icon-watch-1"></i>
+                                    Business <br> Restructuring
+                                </a>
+                            </li>
+                            <li class="">
+                                <a href="#tab-f-7">
+                                    <i class="icon-school-material"></i>
+                                    Business <br> Planning
+                                </a>
+                            </li>
+                        </ul>
+                        <div id="tab-f-1" class="clearfix tab-body-dark" style="display: none;">
+                            <h4>The power to generate, convince, convert &amp; grow.</h4>
+                            <p>Equity placement or recapitalization through our extensive network for a private
+                                placement or Pre
+                                initial public offering</p>
+                        </div>
+                        <div id="tab-f-2" class="clearfix tab-body-dark" style="display: none;">
+                            <h4>Brand builders. Brand managers.</h4>
+                            <p>Provide advice on targets and execution of M&amp;A through a package, search the right
+                                buyer and
+                                lead deal coordination of potential investors through our extensive client network,
+                                evaluation
+                                and recommendation of a fair range of valuations of M&amp;A transaction.</p>
+                        </div>
+                        <div id="tab-f-3" class="clearfix tab-body-dark" style="display: none;">
+                            <h4>Where great brands begin!</h4>
+                            <p>Provides services at all stages of preparing a valuation of share/company based on their
+                                financial records and their perspective.</p>
+                        </div>
+                        <div id="tab-f-4" class="clearfix tab-body-dark" style="display: none;">
+                            <h4>Creating a world of brilliant identities.</h4>
+                            <p>Render the opinion regarding the reasonableness of the transactions and the fairness of
+                                the price
+                                and conditions of the transactions.</p>
+                        </div>
+                        <div id="tab-f-5" class="clearfix tab-body-dark" style="display: block;">
+                            <h4>Forging brands, driving revenue.</h4>
+                            <p>Provides services at all stages of preparing for an IPO and transforming a company from
+                                private
+                                into a public entity.</p>
+                        </div>
+                        <div id="tab-f-6" class="clearfix tab-body-dark" style="display: block;">
+                            <h4>Finding new opportunities to grow</h4>
+                            <p>Provide a full range of services in restructuring, Advise information of a viable
+                                financial
+                                restructuring plan and assist in negotiations with lenders or other parties.</p>
+                        </div>
+                        <div id="tab-f-7" class="clearfix tab-body-dark" style="display: block;">
+                            <h4>Plan, Predict and achieve milestones</h4>
+                            <p>Provides a maximum range of services in the Feasibility study / Business plan which is
+                                used
+                                to determine the viability of an idea, such an ensuring that a project is legally and
+                                technically feasible as well as economically justifiable and also identifies the
+                                possible
+                                constraints in the
+                                project /Plan. We also provide possible solutions for the constraints.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
