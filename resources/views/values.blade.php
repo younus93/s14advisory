fdc8465be300509292e03d91cd9f2bce82f5a124
younus93 <itsme@theyounus.com>
2019-04-10 13:30:25 +0530

    wip

--- resources/views/values.blade.php
+++ resources/views/values.blade.php
@@ -1,15 +1,19 @@
 <div class="section-block" id="values">
     <div class="container">
-        <div class="section-heading center-holder"><h3 class="bold">Our Values</h3>
+        <div class="section-heading center-holder mb-0">
+            <h3 class="bold">
+                Our values reflect through our <span
+                        class="italic libre-baskerville primary-color">S.O.L.U.T.I.O.N</span> philosophy
+            </h3>
             <div class="section-heading-line"></div>
-            <p></p>
         </div>
         <div class="mt-70"></div>
         <div class="timeline">
             <div class="timeline-left-all">
                 <div class="timeline-container timeline-left">
                     <div class="timeline-content">
-                        <div class="timeline-content-inner"><h3>Success</h3>
+                        <div class="timeline-content-inner">
+                            <h3><span class="italic libre-baskerville primary-color">S</span>uccess</h3>
                             <p>We put our heart, mind, and soul into each and every single work. This is the secret
                                 of our success.</p>
                         </div>
@@ -19,7 +23,8 @@
             <div class="timeline-right-all">
                 <div class="timeline-container timeline-right">
                     <div class="timeline-content">
-                        <div class="timeline-content-inner"><h3>Organization</h3>
+                        <div class="timeline-content-inner">
+                            <h3><span class="italic libre-baskerville primary-color">O</span>rganization</h3>
                             <p>We believe that the achievements of an organization are the results of the combined
                                 effort of each individual.</p>
                         </div>
@@ -29,7 +34,8 @@
             <div class="timeline-left-all">
                 <div class="timeline-container timeline-left">
                     <div class="timeline-content">
-                        <div class="timeline-content-inner"><h3>Loyalty</h3>
+                        <div class="timeline-content-inner">
+                            <h3><span class="italic libre-baskerville primary-color">L</span>oyalty</h3>
                             <p>The strength of brand loyalty begins with our service makes our client&#39;s
                                 feelings.</p>
                         </div>
@@ -39,7 +45,8 @@
             <div class="timeline-right-all">
                 <div class="timeline-container timeline-right">
                     <div class="timeline-content">
-                        <div class="timeline-content-inner"><h3>Understanding</h3>
+                        <div class="timeline-content-inner">
+                            <h3><span class="italic libre-baskerville primary-color">U</span>nderstanding</h3>
                             <p>We understand our client&#39;s needs and provide the service accordingly.</p>
                         </div>
                     </div>
@@ -48,7 +55,8 @@
             <div class="timeline-left-all">
                 <div class="timeline-container timeline-left">
                     <div class="timeline-content">
-                        <div class="timeline-content-inner"><h3>Teamwork</h3>
+                        <div class="timeline-content-inner">
+                            <h3><span class="italic libre-baskerville primary-color">T</span>eamwork</h3>
                             <p>We work together to meet the needs of our clients and to help the company win.</p>
                         </div>
                     </div>
@@ -57,7 +65,8 @@
             <div class="timeline-right-all">
                 <div class="timeline-container timeline-right">
                     <div class="timeline-content">
-                        <div class="timeline-content-inner"><h3>Integrity</h3>
+                        <div class="timeline-content-inner">
+                            <h3><span class="italic libre-baskerville primary-color">I</span>ntegrity</h3>
                             <p>We uphold the highest standards of integrity in all our actions.</p>
                         </div>
                     </div>
@@ -66,7 +75,8 @@
             <div class="timeline-left-all">
                 <div class="timeline-container timeline-left">
                     <div class="timeline-content">
-                        <div class="timeline-content-inner"><h3>Outstanding</h3>
+                        <div class="timeline-content-inner">
+                            <h3><span class="italic libre-baskerville primary-color">O</span>utstanding</h3>
                             <p>Our work speaks more than our word, that&#39;s why we are always outstanding in what
                                 we do.</p>
                         </div>
@@ -76,7 +86,8 @@
             <div class="timeline-right-all">
                 <div class="timeline-container timeline-right">
                     <div class="timeline-content">
-                        <div class="timeline-content-inner"><h3>Nimble</h3>
+                        <div class="timeline-content-inner">
+                            <h3><span class="italic libre-baskerville primary-color">N</span>imble</h3>
                             <p>We are quick and neat in what we do.</p>
                         </div>
                     </div>
