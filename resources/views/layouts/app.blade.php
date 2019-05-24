d24408ee4994c8684e61b3e5ce1135388fa8e1a8
younus93 <itsme@theyounus.com>
2019-05-24 10:25:00 +0530

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,16 @@
     <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400i,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900"
           rel="stylesheet">
+          <!--Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140816428-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-140816428-1');
+    </script>
+
     @yield('head')
     <style>
         body{
