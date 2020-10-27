5cefca83e1fd0b52bc9b67c69a2e1cebccc759e9
PeerSyedHussain <syedhussain7558@gmail.com>
2020-10-27 13:39:18 +0530

    oct 26 reviewwip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,34 @@
 <head>
     <title>S14Advisory</title>
     <meta charset="UTF-8">
-    <meta name="description" content="S14 Advisory Company Limited is the SEC certified in March 2016 and proudly serves the nation's leading companies, small to medium-sized businesses, and other institutions">
+    <!-- <meta name="description" content="S14 Advisory Company Limited is the SEC certified in March 2016 and proudly serves the nation's leading companies, small to medium-sized businesses, and other institutions">
     <meta property="og:title" content="s14advisory" />
     <meta property="og:url" content="https://s14advisory.com" />
     <meta property="og:description" content="S14 Advisory Company Limited is the SEC certified in March 2016 and proudly serves the nation's leading companies, small to medium-sized businesses, and other institutions">
+    <meta property="og:image" content="/img/logos/logo-preview.png"> -->
+
+    <!-- fb & Whatsapp -->
+
+    <!-- Site Name, Title, and Description to be displayed -->
+    <meta property="og:site_name" content="S14Advisory">
+    <meta property="og:title" content="S14Advisory">
+    <meta property="og:description" content="S14 Advisory Company Limited is the SEC certified in March 2016 and proudly serves the nation's leading companies, small to medium-sized businesses, and other institutions">
+
+    <!-- Image to display -->
+    <!-- Replace   «example.com/image01.jpg» with your own -->
     <meta property="og:image" content="/img/logos/logo-preview.png">
+
+    <!-- No need to change anything here -->
+    <meta property="og:type" content="website" />
+    <meta property="og:image:type" content="image/jpeg">
+
+    <!-- Size of image. Any size up to 300. Anything above 300px will not work in WhatsApp -->
+    <meta property="og:image:width" content="90">
+    <meta property="og:image:height" content="90">
+
+    <!-- Website to visit when clicked in fb or WhatsApp-->
+    <meta property="og:url" content="https://s14advisory.com">
+
     <link rel="shortcut icon" href="/img/logos/logo-preview.png"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
